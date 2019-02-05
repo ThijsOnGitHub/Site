0b27b8e1b2ddf34db10595476e189f6914c1ea63
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-02-05 16:17:51 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,11 +2,12 @@ var tekst=document.getElementById("tekst")
 var knop= document.getElementById("knop")
 tekstvarianten=["Dit is magie, deze tekst is veranderd","Leuk dat je klikte","Nu lees je hier wat anders","ERROR 404"]
 function randomInt(min,max) {
-    return Math.floor(Math.random()*max*10)+min
+    return Math.floor(Math.random()*max*1)+min
 }
 
 knop.addEventListener("click",function (){
-alert("Let's change some tekst")
-deNieuweTekst=tekstvarianten[randomInt(0,tekstvarianten.length)]
-tekst.innerHTML=deNieuweTekst
+    console.log(randomInt(0,10))
+    alert("Let's change some tekst")
+    deNieuweTekst=tekstvarianten[randomInt(0,tekstvarianten.length)]
+    tekst.innerHTML=deNieuweTekst
 })
