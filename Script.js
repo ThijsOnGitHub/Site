8baa1b497598c404545bc753af687d534ea50bf2
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-02-05 13:05:32 +0100

    Tekst veranderd nu door klikken op knop

--- Script.js
+++ Script.js
@@ -1,2 +1,6 @@
 var tekst=document.getElementById("tekst")
-tekst.innerHTML="Hallo";
+var knop= document.getElementById("knop")
+knop.addEventListener("click",function (){
+alert("Let's change some tekst")
+tekst.innerHTML="Hallo deze tekst is veranderd"
+})
